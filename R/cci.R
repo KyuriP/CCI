984bf12a0920932d586c6daca5dcc2682af9ac36
Kyuri <kyurheep@gmail.com>
2023-01-21 13:52:20 +0100

    Update cci.R

--- R/cci.R
+++ R/cci.R
@@ -63,7 +63,7 @@ cci <- function (suffStat, indepTest, alpha, p, skeleton_pre=NULL,
                    verbose = verbose, rules_used = list_f$rules_used)
 
   # make sure they are numeric matrix (when only 0, 1 exist it turns into logical values)
-  apply(res$pag, 2, as.numeric)
+  res$pag <- apply(res$pag, 2, as.numeric)
   # label the pag
   dimnames(res$pag) <- list(labels, labels)
   return(list(maag = res$pag, pre_OR_res = list_f$pag, rules_used = res$rules_used, sepset=sepset))
