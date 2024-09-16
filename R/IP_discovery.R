d6ac9759ecc740c3bd77574028edd04c29018e72
Kyuri <kyurheep@gmail.com>
2024-09-16 15:27:18 +0200

    update IPdiscoovery

--- R/IP_discovery.R
+++ R/IP_discovery.R
@@ -4,26 +4,42 @@
 #' @param alpha Type I error rate for indepTest
 #' @return List containing the skeleton data
 
-IP_discovery <- function(suffStat,indepTest,alpha, p,max.cs=Inf){
-
-  time_start <- proc.time();
-  skel <- skeleton_new(suffStat, indepTest, alpha, p = p, m.max=max.cs)
-
+IP_discovery <- function(suffStat, indepTest, alpha, p, max.cs = Inf, contextVars = NULL, jci = c("0", "1", "123"), verbose = FALSE) {
+  
+  # Start timing the process
+  time_start <- proc.time()
+  
+  # Run the skeleton discovery using the modified skeleton function that incorporates JCI assumptions
+  skel <- skeleton_new_jci(suffStat, indepTest, alpha, p = p, m.max = max.cs, contextVars = contextVars, jci = jci, verbose = verbose)
+  
+  # Extract the graph and separation sets from the skeleton phase
   G_sk <- as(skel@graph, "matrix")
   sepset_sk <- skel@sepset
-
-  time_skel = proc.time()-time_start;
-
-  pdsepRes <- pdsep(skel@graph, suffStat, indepTest,
-                    p=p, sepset_sk, alpha, skel@pMax, m.max=max.cs)
-
+  
+  # Measure time for the skeleton phase
+  time_skel <- proc.time() - time_start
+  
+  # Perform the possible-d-separation (pdsep) step based on the skeleton
+  pdsepRes <- pdsep(skel@graph, suffStat, indepTest, p = p, sepset = sepset_sk, alpha = alpha, pMax = skel@pMax, m.max = max.cs, verbose = verbose)
+  
+  # Extract the final graph and separation sets after the pdsep phase
   G <- pdsepRes$G
   sepset <- pdsepRes$sepset
-
-  time_pdsep = proc.time()-time_start;
-
-  resIP <- list(G=G,G_sk=G_sk, sepset=sepset, sepset_sk = sepset_sk, time_skel=time_skel, time_pdsep=time_pdsep, skel=skel, pdsepRes=pdsepRes)
-
+  
+  # Measure time for the entire process
+  time_pdsep <- proc.time() - time_start
+  
+  # Store the results in a list and return
+  resIP <- list(
+    G = G,
+    G_sk = G_sk,
+    sepset = sepset,
+    sepset_sk = sepset_sk,
+    time_skel = time_skel,
+    time_pdsep = time_pdsep,
+    skel = skel,
+    pdsepRes = pdsepRes
+  )
+  
   return(resIP)
-
 }
--- R/skeleton_new.R
+++ R/skeleton_new.R
@@ -1,60 +1,82 @@
-skeleton_new <- function (suffStat, indepTest, alpha, labels=NULL, p, method = c("stable",
-                                                            "original", "stable.fast"), m.max = Inf, fixedGaps = NULL,
-          fixedEdges = NULL, NAdelete = TRUE, verbose = FALSE)
-{
-
+skeleton_new_jci <- function (suffStat, indepTest, alpha, labels = NULL, p, 
+                              method = c("stable", "original", "stable.fast"), 
+                              m.max = Inf, fixedGaps = NULL, fixedEdges = NULL, 
+                              NAdelete = TRUE, verbose = FALSE, 
+                              contextVars = NULL, jci = c("0", "1", "123")) {
+  
   cl <- match.call()
+  
+  # Check if 'p' is provided and valid
   if (!missing(p))
-    stopifnot(is.numeric(p), length(p <- as.integer(p)) ==
-                1, p >= 2)
-
+    stopifnot(is.numeric(p), length(p <- as.integer(p)) == 1, p >= 2)
+  
+  # Handle labels and p consistency
   if (missing(labels)) {
-    if (missing(p))
-      stop("need to specify 'labels' or 'p'")
+    if (missing(p)) stop("need to specify 'labels' or 'p'")
     labels <- as.character(seq_len(p))
-  }
-  else {
+  } else {
     stopifnot(is.character(labels))
-    if (missing(p))
-      p <- length(labels)
-    else if (p != length(labels))
-      stop("'p' is not needed when 'labels' is specified, and must match length(labels)")
+    if (missing(p)) p <- length(labels)
+    else if (p != length(labels)) stop("'p' must match length of labels")
   }
+  
   seq_p <- seq_len(p)
   method <- match.arg(method)
+  
+  # Create initial adjacency matrix G (TRUE where we will investigate edges)
   if (is.null(fixedGaps)) {
     G <- matrix(TRUE, nrow = p, ncol = p)
-  }
-  else if (!identical(dim(fixedGaps), c(p, p)))
+  } else if (!identical(dim(fixedGaps), c(p, p))) {
     stop("Dimensions of the dataset and fixedGaps do not agree.")
-  else if (!identical(fixedGaps, t(fixedGaps)))
+  } else if (!identical(fixedGaps, t(fixedGaps))) {
     stop("fixedGaps must be symmetric")
-  else G <- !fixedGaps
+  } else G <- !fixedGaps
+  
   diag(G) <- FALSE
+  
+  # Handle fixed edges, with JCI Assumptions
   if (any(is.null(fixedEdges))) {
-    fixedEdges <- matrix(rep(FALSE, p * p), nrow = p, ncol = p)
-  }
-  else if (!identical(dim(fixedEdges), c(p, p)))
+    fixedEdges <- matrix(FALSE, p, p)
+  } else if (!identical(dim(fixedEdges), c(p, p))) {
     stop("Dimensions of the dataset and fixedEdges do not agree.")
-  else if (!identical(fixedEdges, t(fixedEdges)))
+  } else if (!identical(fixedEdges, t(fixedEdges))) {
     stop("fixedEdges must be symmetric")
+  }
+  
+  # Handle JCI Assumptions:
+  jci <- match.arg(jci)
+  if (jci == "123" && length(contextVars) > 0) {
+    # Set fixed edges between context variables for JCI Assumption 3
+    fixedEdges[contextVars, contextVars] <- TRUE
+  }
+  
+  if (jci %in% c("1", "123") && length(contextVars) > 0) {
+    # Direct all edges between context and system variables (JCI Assumption 1 and 2)
+    for (i in contextVars) {
+      for (j in setdiff(seq_len(p), contextVars)) {
+        if (jci == "123") {
+          fixedEdges[i, j] <- TRUE  # k -> i for JCI123
+          G[i, j] <- FALSE          # No need to test this edge
+        } else if (jci == "1") {
+          fixedEdges[i, j] <- TRUE  # k -> i for JCI1
+        }
+      }
+    }
+  }
+  
+  # Use fast skeleton estimation if requested
   if (method == "stable.fast") {
-    if (identical(indepTest, gaussCItest))
-      indepTestName <- "gauss"
+    if (identical(indepTest, gaussCItest)) indepTestName <- "gauss"
     else indepTestName <- "rfun"
-    options <- list(verbose = as.integer(verbose), m.max = as.integer(ifelse(is.infinite(m.max),
-                                                                             p, m.max)), NAdelete = NAdelete)
-    res <- .Call("estimateSkeleton", G, suffStat, indepTestName,
-                 indepTest, alpha, fixedEdges, options)
+    options <- list(verbose = as.integer(verbose), m.max = as.integer(ifelse(is.infinite(m.max), p, m.max)), NAdelete = NAdelete)
+    res <- .Call("estimateSkeleton", G, suffStat, indepTestName, indepTest, alpha, fixedEdges, options)
     G <- res$amat
-    sepset <- lapply(seq_p, function(i) c(lapply(res$sepset[[i]],
-                                                 function(v) if (identical(v, as.integer(-1))) NULL else v),
-                                          vector("list", p - length(res$sepset[[i]]))))
+    sepset <- lapply(seq_p, function(i) c(lapply(res$sepset[[i]], function(v) if (identical(v, as.integer(-1))) NULL else v), vector("list", p - length(res$sepset[[i]]))))
     pMax <- res$pMax
     n.edgetests <- res$n.edgetests
     ord <- length(n.edgetests) - 1L
-  }
-  else {
+  } else {
+    # Original skeleton estimation (non-fast)
     pval <- NULL
     sepset <- lapply(seq_p, function(.) vector("list", p))
     pMax <- matrix(-Inf, nrow = p, ncol = p)
@@ -69,20 +91,17 @@ skeleton_new <- function (suffStat, indepTest, alpha, labels=NULL, p, method = c
       ind <- ind[order(ind[, 1]), ]
       remEdges <- nrow(ind)
       if (verbose)
-        cat("Order=", ord, "; remaining edges:", remEdges,
-            "\n", sep = "")
+        cat("Order=", ord, "; remaining edges:", remEdges, "\n", sep = "")
       if (method == "stable") {
         G.l <- split(G, gl(p, p))
       }
       for (i in 1:remEdges) {
-        if (verbose && (verbose >= 2 || i%%100 == 0))
+        if (verbose && (verbose >= 2 || i %% 100 == 0))
           cat("|i=", i, "|iMax=", remEdges, "\n")
         x <- ind[i, 1]
         y <- ind[i, 2]
         if (G[y, x] && !fixedEdges[y, x]) {
-          nbrsBool <- if (method == "stable")
-            G.l[[x]]
-          else G[, x]
+          nbrsBool <- if (method == "stable") G.l[[x]] else G[, x]
           nbrsBool[y] <- FALSE
           nbrs <- seq_p[nbrsBool]
           length_nbrs <- length(nbrs)
@@ -91,12 +110,10 @@ skeleton_new <- function (suffStat, indepTest, alpha, labels=NULL, p, method = c
               done <- FALSE
             S <- seq_len(ord)
             repeat {
-              n.edgetests[ord1] <- n.edgetests[ord1] +
-                1
+              n.edgetests[ord1] <- n.edgetests[ord1] + 1
               pval <- indepTest(x, y, nbrs[S], suffStat)
               if (verbose)
-                cat("x=", x, " y=", y, " S=", nbrs[S],
-                    ": pval =", pval, "\n")
+                cat("x=", x, " y=", y, " S=", nbrs[S], ": pval =", pval, "\n")
               if (is.na(pval))
                 pval <- as.numeric(NAdelete)
               if (pMax[x, y] < pval)
@@ -105,10 +122,8 @@ skeleton_new <- function (suffStat, indepTest, alpha, labels=NULL, p, method = c
                 G[x, y] <- G[y, x] <- FALSE
                 sepset[[x]][[y]] <- sepset[[y]][[x]] <- nbrs[S]
                 break
-              }
-              else {
-                nextSet <- getNextSet(length_nbrs, ord,
-                                      S)
+              } else {
+                nextSet <- getNextSet(length_nbrs, ord, S)
                 if (nextSet$wasLast)
                   break
                 S <- nextSet$nextSet
@@ -120,17 +135,18 @@ skeleton_new <- function (suffStat, indepTest, alpha, labels=NULL, p, method = c
       ord <- ord + 1L
     }
     for (i in 1:(p - 1)) {
-      for (j in 2:p) pMax[i, j] <- pMax[j, i] <- max(pMax[i,
-                                                          j], pMax[j, i])
+      for (j in 2:p)
+        pMax[i, j] <- pMax[j, i] <- max(pMax[i, j], pMax[j, i])
     }
   }
+  
   Gobject <- if (sum(G) == 0) {
     new("graphNEL", nodes = labels)
-  }
-  else {
+  } else {
     colnames(G) <- rownames(G) <- labels
     as(G, "graphNEL")
   }
+  
   new("pcAlgo", graph = Gobject, call = cl, n = integer(0),
       max.ord = as.integer(ord - 1), n.edgetests = n.edgetests,
       sepset = sepset, pMax = pMax, zMin = matrix(NA, 1, 1))
